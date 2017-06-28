e281b2a36dcaa63f92e7e1e095a09901d8fc958d
Antonia Cerda <antoniacerdag@gmail.com>
2017-06-28 13:45:00 -0400

    basurero

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
 	
-	$( "#tarea-input" ).keypress(function() {
-		var tareaNueva = $(".input-box").val();
-		$(".tareas-nuevas").append(tareaNueva);
-  		
-
+	$( "#tarea-input" ).keypress(function(e) {
+		var key = e.which;
+		if (key == 13) {
+			var tareaNueva = $(".input-box").val();
+			var btnRemove = $(".tareas-nuevas").append("")
+			var nuevaTarea = $(".tareas-nuevas").append("<p> <input type='checkbox' class='test5'/><label>" + tareaNueva + "</label> <a href='#' class='btn-basurero' type='button'><i class='fa fa-trash-o' aria-hidden='true'></i></a> </p>");
+		}
+	
 	});
 })
