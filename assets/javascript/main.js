06d1e7aeb6231fd0f46a30ff66858a7f595522a8
Antonia Cerda <antoniacerdag@gmail.com>
2017-06-28 18:28:32 -0400

    funcion para que elimine comentario

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,13 +1,25 @@
 $(document).ready(function(){
 	
+	$('#list-items').html(localStorage.getItem('listItems'));
+
 	$("#tarea-input" ).keypress(function(e) {
 		var key = e.which;
 		if (key == 13) {
+
 			var tareaNueva = $(".input-box").val();
-			var nuevaTarea = $(".tareas-nuevas").append("<p> <input type='checkbox' class='test5 checkbox'/><label>" + tareaNueva + "</label> <a href='#' class='btn-basurero' type='button'><i class='fa fa-trash-o' aria-hidden='true'></i></a> </p>");
+			var nuevaTarea = $(".tareas-nuevas").append("<p> <input type='checkbox' id='test5 checkbox'/><label>" + tareaNueva + "</label> <a href='' class='btn-basurero' type='button'><i class='fa fa-trash-o' aria-hidden='true'></i></a> </p>");
+			$(".input-box").val(" ");
+
 			$(".checkbox").click(function(m){
-				$(nuevaTarea).append(".tareas-completas");
+				var tareaNueva = $(".input-box").val();
+			$(m).append(nuevaTarea);
 			});
+
+			$(".btn-basurero").click(function(r){
+				$(r).remove();
+			})
 		}
 	});
+
+
 })
