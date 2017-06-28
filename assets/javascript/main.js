285b71a52edc481674c5f4e714938d8476b50b1c
Antonia Cerda <antoniacerdag@gmail.com>
2017-06-28 13:54:41 -0400

    click on checkbox

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 	
-	$( "#tarea-input" ).keypress(function(e) {
+	$("#tarea-input" ).keypress(function(e) {
 		var key = e.which;
 		if (key == 13) {
 			var tareaNueva = $(".input-box").val();
-			var btnRemove = $(".tareas-nuevas").append("")
-			var nuevaTarea = $(".tareas-nuevas").append("<p> <input type='checkbox' class='test5'/><label>" + tareaNueva + "</label> <a href='#' class='btn-basurero' type='button'><i class='fa fa-trash-o' aria-hidden='true'></i></a> </p>");
+			var nuevaTarea = $(".tareas-nuevas").append("<p> <input type='checkbox' class='test5 checkbox'/><label>" + tareaNueva + "</label> <a href='#' class='btn-basurero' type='button'><i class='fa fa-trash-o' aria-hidden='true'></i></a> </p>");
+			$(".checkbox").click(function(m){
+				$(nuevaTarea).append(".tareas-completas");
+			});
 		}
-	
 	});
 })
